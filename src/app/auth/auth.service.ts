2933b524547a2615c3bfeb6d1d95a25fe9f58e75
Sayan Dey <sayandey450@gmail.com>
2024-08-16 17:38:17 +0530

    name persistance fix

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,10 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import {
-  Injectable,
-  WritableSignal,
-  inject,
-  signal,
-} from '@angular/core';
+import { Injectable, WritableSignal, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Injectable({
@@ -19,14 +14,14 @@ export class AuthService {
   private readonly name: WritableSignal<string> = signal('--');
 
   get isAuthenticated() {
-    if(localStorage.getItem("token")) {
+    if (localStorage.getItem('token')) {
       this.authenticated.set(true);
     }
     return this.authenticated;
   }
 
   get getName() {
-    this.name.set(localStorage.getItem("name") || '--');
+    this.name.set(localStorage.getItem('name') || '--');
     return this.name;
   }
 
@@ -34,8 +29,12 @@ export class AuthService {
     this.http
       .post(this.uri + 'login', { username, password })
       .subscribe((data: any) => {
-        if(data) Object.keys(data).forEach((key) => localStorage.setItem(key, data[key]));
+        if (data)
+          Object.keys(data).forEach((key) =>
+            localStorage.setItem(key, data[key])
+          );
         this.isAuthenticated.set(true);
+        if (data.name) this.name.set(data.name);
         this.router.navigateByUrl('/land-record');
       });
   }
@@ -43,6 +42,7 @@ export class AuthService {
   logout(): void {
     localStorage.clear();
     this.isAuthenticated.set(false);
+    this.name.set('--');
     this.router.navigateByUrl('/login');
   }
 
