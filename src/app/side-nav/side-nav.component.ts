9532c44dbf08d067d3f8b03151492aebbf34c644
Archisman Tripathi <archismantripathi@gmail.com>
2024-09-04 13:21:36 +0530

    add nav items

--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -1,8 +1,16 @@
-import { Component, inject, Input, signal, WritableSignal } from '@angular/core';
+import {
+  Component,
+  inject,
+  Input,
+  OnInit,
+  signal,
+  WritableSignal,
+} from '@angular/core';
 import { MatRippleModule } from '@angular/material/core';
 import { MatListModule } from '@angular/material/list';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
+import { NavItem } from '../model/nav-item.model';
 
 @Component({
   selector: 'app-side-nav',
@@ -11,7 +19,19 @@ import { AuthService } from '../auth/auth.service';
   templateUrl: './side-nav.component.html',
   styleUrl: './side-nav.component.scss',
 })
-export class SideNavComponent {
+export class SideNavComponent implements OnInit {
   @Input() navState: WritableSignal<boolean> = signal(true);
   isAdmin: WritableSignal<boolean> = inject(AuthService).isAdmin;
+  navItems: WritableSignal<NavItem[]> = signal([]);
+
+  ngOnInit(): void {
+    this.navItems.set([
+      { title: 'User Master', link: '/master/user', adminOnly: true },
+      { title: 'Company Master', link: '/master/company', adminOnly: true },
+      { title: 'Group Master', link: '/master/group', adminOnly: true },
+      { title: 'Mouza Master', link: '/master/mouza', adminOnly: true },
+      { title: 'Land Record', link: '/land-record', adminOnly: false },
+      // { title: 'Report', link: '/report', adminOnly: false },
+    ]);
+  }
 }
