18bdb5fe0f0a34eeb03250733571165798809aaf
Archisman Tripathi <archismantripathi@gmail.com>
2024-09-04 16:01:33 +0530

    add company editor modal component

--- src/app/main/master/company-master/company-master.component.ts
+++ src/app/main/master/company-master/company-master.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-company-master',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatCardModule, MatTableModule, RouterLink],
   templateUrl: './company-master.component.html',
-  styleUrl: './company-master.component.scss'
+  styleUrl: './company-master.component.scss',
 })
 export class CompanyMasterComponent {
+  companyList: WritableSignal<any[]> = signal([]);
+  displayedColumns: WritableSignal<string[]> = signal([
+    'slno',
+    'name',
+    'action',
+  ]);
+  onDeleteCompany(arg0: any) {
+    throw new Error('Method not implemented.');
+  }
+  onUpdateCompany(arg0: any) {
+    throw new Error('Method not implemented.');
+  }
 
+  onNewCompany() {
+    throw new Error('Method not implemented.');
+  }
 }
---
+++ src/app/main/master/company-master/modal/company-editor/company-editor.dialog.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dialog-company',
+  standalone: true,
+  imports: [],
+  templateUrl: './company-editor.dialog.html',
+  styleUrl: './company-editor.dialog.scss'
+})
+export class DialogCompanyEditorComponent {
+
+}
