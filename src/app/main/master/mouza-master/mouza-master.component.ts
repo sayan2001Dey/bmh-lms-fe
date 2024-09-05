e93f90595cd26af94ba73efaeaee14e4fd1e7048
bmhtech <bmhtech.pvt@gmail.com>
2024-09-05 14:15:51 +0530

    Added mouza master

--- src/app/main/master/mouza-master/mouza-master.component.ts
+++ src/app/main/master/mouza-master/mouza-master.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-mouza-master',
   standalone: true,
-  imports: [],
+  imports: [
+   MatFormFieldModule,
+   MatInputModule,
+   ReactiveFormsModule,
+   MatDatepickerModule,
+   MatSlideToggleModule,
+   MatRadioModule,
+   MatDividerModule,
+   MatListModule,
+   MatTableModule,
+   MatCardModule,
+   MatButton
+
+  ],
   templateUrl: './mouza-master.component.html',
   styleUrl: './mouza-master.component.scss'
 })
 export class MouzaMasterComponent {
+
 
 }
