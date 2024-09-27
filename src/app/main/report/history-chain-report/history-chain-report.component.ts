3a78fac7a2f94e6522631db32c6eb250d248b554
bmhtech <bmhtech.pvt@gmail.com>
2024-09-27 17:49:50 +0530

    Add history chain graph

--- src/app/main/report/history-chain-report/history-chain-report.component.ts
+++ src/app/main/report/history-chain-report/history-chain-report.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-history-chain-report',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatTooltipModule,
+    MatButtonModule,
+    MatCardModule,
+  ],
   templateUrl: './history-chain-report.component.html',
   styleUrl: './history-chain-report.component.scss'
 })
 export class HistoryChainReportComponent {
+value: any;
 
 }
