30e0ac8ab5c41c70e9c3aeaa949f1fde2e7a438a
Archisman Tripathi <archismantripathi@gmail.com>
2024-10-24 14:00:29 +0530

    cp 0

--- src/app/main/report/history-chain-report/history-chain-report.component.ts
+++ src/app/main/report/history-chain-report/history-chain-report.component.ts
@@ -24,7 +24,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { TestReportComponent } from '../test-report/test-report.component';
 import { HistoryChainData } from '../../../model/history-chain-data.model';
 import { GraphStateData } from '../../../model/graph-state-data.model';
-import { HistoryChainGraphDynamicComponent } from "../history-chain-graph/history-chain-graph-dynamic/history-chain-graph-dynamic.component";
+import { HistoryChainGraphDynamicComponent } from '../history-chain-graph/history-chain-graph-dynamic/history-chain-graph-dynamic.component';
 
 @Component({
   selector: 'app-history-chain-report',
@@ -41,15 +41,13 @@ import { HistoryChainGraphDynamicComponent } from "../history-chain-graph/histor
     AsyncPipe,
     HistoryChainGraphComponent,
     TestReportComponent,
-    HistoryChainGraphDynamicComponent
-],
+    HistoryChainGraphDynamicComponent,
+  ],
   templateUrl: './history-chain-report.component.html',
   styleUrl: './history-chain-report.component.scss',
 })
 export class HistoryChainReportComponent implements OnInit {
   private readonly deedService: DeedMasterService = inject(DeedMasterService);
-  private readonly landRecordService: LandRecordsService =
-    inject(LandRecordsService);
   private readonly historyChainReportService: HistoryChainReportService =
     inject(HistoryChainReportService);
   readonly searchBar: FormControl<string | null> =
@@ -100,7 +98,8 @@ export class HistoryChainReportComponent implements OnInit {
       return;
     }
     const deedId =
-      this.deedList().find((deed) => deed.deedNo === searchBarVal)?.deedId || '';
+      this.deedList().find((deed) => deed.deedNo === searchBarVal)?.deedId ||
+      '';
 
     if (deedId === '') {
       alert('⛔ ERROR: UNKNOWN INPUT\n\nDeed not found.');
@@ -137,6 +136,51 @@ export class HistoryChainReportComponent implements OnInit {
     let nodesData: any[] = [];
     let linkData: any[] = [];
 
+    const layoutData = this.processLayout2(historyChainDataList);
+    console.log('layoutData', layoutData);
+
+    // /*
+    let rowLayoutData: GraphRowLayoutMatrix =
+      this.processLayout(historyChainDataList);
+    let data = rowLayoutData.data;
+
+    let x = 0;
+    let y = 0;
+
+    //DEBUG
+    console.log(
+      'Test: ' +
+        rowLayoutData.maxWidth / 2 +
+        ', data.length:' +
+        data.length +
+        ', data[i].length:' +
+        data[1].length
+    );
+    for (let i = 0; i < data.length; i++, x++) {
+      for (let j = 0; j < data[i].length; j++) {
+        let d = data[i][j];
+        const node: any = {
+          key: d.deedId,
+          text: this.getDeedNo(d.deedId) + '\n' + d.deedId,
+          color: d.deedId === this.highlightDeedId ? 'orange' : 'lightgreen',
+          loc: j * 100 + ' ' + i * 120,
+        };
+        if (i === 0)
+          node.loc = (rowLayoutData.maxWidth / 2) * 100 + ' ' + x * 120;
+        nodesData.push(node);
+
+        for (const parentId of d.parents) {
+          linkData.push({
+            key: -linkData.length - 1,
+            from: parentId,
+            to: d.deedId,
+          });
+        }
+      }
+    }
+    // */
+
+    /*
     for (let i = 0; i < historyChainDataList.length; i++) {
       let d = historyChainDataList[i];
       nodesData.push({
@@ -154,6 +198,7 @@ export class HistoryChainReportComponent implements OnInit {
         });
       }
     }
+    */
 
     this.stateSignal.set(
       (() => {
@@ -163,6 +208,88 @@ export class HistoryChainReportComponent implements OnInit {
         return d;
       })()
     );
+  }
+
+  processLayout2(
+    hcdList: HistoryChainData[],
+    parentDeedId: string | null = null,
+    incurredNodes: Set<string> = new Set()
+  ): HCGraphLayoutTreeNode[] {
+    const res: HCGraphLayoutTreeNode[] = [];
+
+    for (let i = 0; i < hcdList.length; i++) {
+      const hcd = hcdList[i];
+      console.log('hcd', hcd);
+      if (
+        (parentDeedId === null && hcd.parents.length === 0) ||
+        (parentDeedId !== null && hcd.parents.includes(parentDeedId))
+      ) {
+        res.push({
+          deedId: hcd.deedId,
+          depth: 0,
+          width: 0,
+          children: [],
+        });
+        incurredNodes.add(hcd.deedId);
+      }
+    }
+
+    for (let i = 0; i < res.length; i++) {
+      const tmp = res[i];
+      const tmpIncurredNodes: Set<string> = new Set();
+      tmp.children = this.processLayout2(hcdList, tmp.deedId, tmpIncurredNodes);
+      if (tmp.children.length === 0) {
+        tmp.width = 1;
+        tmp.depth = 1;
+      } else {
+        tmp.width = tmp.children.reduce((prev, curr) => prev + curr.width, 0);
+        tmp.depth =
+          tmp.children.reduce((prev, curr) => {
+            return Math.max(prev, curr.depth);
+          }, 0) + 1;
+        // From where do i remove?
+        // fml
+        incurredNodes.forEach((id) => {
+          // How?
+          // fml 2.0
+        });
+      }
+    }
+
+    //DEBUG
+    console.clear();
+    console.log(res);
+    return res;
+  }
+
+  processLayout(hcdList: HistoryChainData[]): GraphRowLayoutMatrix {
+    this.processLayout2(hcdList);
+    let layoutMatrix: GraphRowLayoutMatrix = {
+      maxWidth: 0,
+      data: [],
+    };
+    layoutMatrix.data.push([]);
+
+    for (let i = 0; i < hcdList.length; i++) {
+      // row 0
+      if (hcdList[i].parents.length === 0) {
+        layoutMatrix.data[0].push(hcdList[i]);
+        hcdList.splice(i, 1);
+      }
+    }
+    layoutMatrix.maxWidth = layoutMatrix.data[0].length - 1;
+
+    if (hcdList.length !== 0) {
+      layoutMatrix.data.push([]);
+      layoutMatrix.data[1] = hcdList;
+    }
+
+    layoutMatrix.maxWidth = Math.max(
+      layoutMatrix.maxWidth,
+      layoutMatrix.data[1].length - 1
+    );
+
+    return layoutMatrix;
   }
 
   getDeedNo(deedId: string): string {
@@ -180,4 +307,16 @@ export class HistoryChainReportComponent implements OnInit {
   ngOnInit(): void {
     this.setDeedList();
   }
+}
+
+interface GraphRowLayoutMatrix {
+  maxWidth: number;
+  data: HistoryChainData[][];
+}
+
+interface HCGraphLayoutTreeNode {
+  deedId: string;
+  depth: number;
+  width: number;
+  children: HCGraphLayoutTreeNode[];
 }
